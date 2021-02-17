a73b69929683c7e2c77b4f2fb9eed65939d51238
Fooxiie <dev.fooxiie@gmail.com>
2021-02-17 20:01:36 +0100

    Ajout & modif

--- resources/[esx]/[non-esx]/seatbelt/main.lua
+++ resources/[esx]/[non-esx]/seatbelt/main.lua
@@ -1,4 +1,4 @@
-local isUiOpen = false 
+local isUiOpen = false
 local speedBuffer  = {}
 local velBuffer    = {}
 local beltOn       = false
@@ -7,7 +7,7 @@ local wasInCar     = false
 IsCar = function(veh)
         local vc = GetVehicleClass(veh)
         return (vc >= 0 and vc <= 7) or (vc >= 9 and vc <= 12) or (vc >= 17 and vc <= 20)
-        end 
+        end
 
 Fwv = function (entity)
         local hr = GetEntityHeading(entity) + 90.0
@@ -15,14 +15,14 @@ Fwv = function (entity)
         hr = hr * 0.0174533
         return { x = math.cos(hr) * 2.0, y = math.sin(hr) * 2.0 }
       end
- 
+
 	Citizen.CreateThread(function()
 	while true do
 	Citizen.Wait(0)
-  
+
     local ped = GetPlayerPed(-1)
     local car = GetVehiclePedIsIn(ped)
-    
+
     if car ~= 0 and (wasInCar or IsCar(car)) then
       wasInCar = true
              if isUiOpen == false and not IsPlayerDead(PlayerId()) then
@@ -32,7 +32,7 @@ Fwv = function (entity)
                 isUiOpen = true
             end
 
-       if beltOn then 
+       if beltOn then
 	  DisableControlAction(0, 75, true)  -- Disable exit vehicle when stop
 	  DisableControlAction(27, 75, true) -- Disable exit vehicle when Driving
 	  end
@@ -41,13 +41,13 @@ Fwv = function (entity)
       speedBuffer[1] = GetEntitySpeed(car)
 
 
-      
-      if speedBuffer[2] ~= nil 
+
+      if speedBuffer[2] ~= nil
          and not beltOn
-         and GetEntitySpeedVector(car, true).y > 1.0  
-         and speedBuffer[1] > 19.25 
+         and GetEntitySpeedVector(car, true).y > 1.0
+         and speedBuffer[1] > 19.25
          and (speedBuffer[2] - speedBuffer[1]) > (speedBuffer[1] * 0.255) then
-         
+
         local co = GetEntityCoords(ped)
         local fw = Fwv(ped)
         SetEntityCoords(ped, co.x + fw.x, co.y + fw.y, co.z - 0.47, true, true, true)
@@ -55,37 +55,35 @@ Fwv = function (entity)
         Citizen.Wait(1)
         SetPedToRagdoll(ped, 1000, 1000, 0, 0, 0, 0)
       end
-        
+
       velBuffer[2] = velBuffer[1]
       velBuffer[1] = GetEntityVelocity(car)
-        
+
       if IsControlJustReleased(0, 311) and GetLastInputMethod(0) then
     TriggerEvent("FakeRevive")
 RegisterNetEvent("FakeRevive")
-AddEventHandler("FakeRevive", function(inputText) 
+AddEventHandler("FakeRevive", function(inputText)
 RequestAnimDict("oddjobs@taxi@cyi")
 TaskPlayAnim(GetPlayerPed(-1),"oddjobs@taxi@cyi", "std_hand_off_ps_passenger", 8.0, 8.0, -1, 48, 1, false, false, false)
 end)
-        beltOn = not beltOn 
-        if beltOn then 
+        beltOn = not beltOn
+        if beltOn then
       TriggerEvent("seatbelt:notify", "Ceinture de sécurité ~g~Attachée")
-		  TriggerServerEvent('InteractSound_SV:PlayWithinDistance', 0.5, 'buckle', 0.2)
-			
+
 		  SendNUIMessage({
 		    displayWindow = 'false'
 		    })
-		  isUiOpen = true 
-		else 
+		  isUiOpen = true
+		else
       TriggerEvent("seatbelt:notify", "Ceinture de sécurité ~r~Détachée")
-		  TriggerServerEvent('InteractSound_SV:PlayWithinDistance', 0.5, 'unbuckle', 0.2)
 
 		  SendNUIMessage({
 		     displayWindow = 'true'
 		     })
-		  isUiOpen = true  
+		  isUiOpen = true
 		end
       end
-      
+
     elseif wasInCar then
       wasInCar = false
       beltOn = false
@@ -94,10 +92,10 @@ end)
                 SendNUIMessage({
             	   displayWindow = 'false'
             	   })
-                isUiOpen = false 
+                isUiOpen = false
             end
     end
-    
+
   end
 end)
 
@@ -109,21 +107,9 @@ Citizen.CreateThread(function()
                     displayWindow = 'false'
                })
             isUiOpen = false
-        end    
+        end
 
     end
-end)
-
-Citizen.CreateThread(function()
-  while true do
-  Citizen.Wait(0)
-    if not beltOn and wasInCar == true then
-
-                TriggerServerEvent('InteractSound_SV:PlayWithinDistance', 0.3, 'seatbelt', 0.2)
-                Citizen.Wait(9000)
-    end
-
-end
 end)
 
 RegisterNetEvent("seatbelt:notify")
